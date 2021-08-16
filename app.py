3c1c50f8e8699b5e0515ba27dad4d097a13e33d6
Sundas Noreen <68951712+SundasNoreen@users.noreply.github.com>
2021-08-16 22:53:11 +0500

    bnm

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from functools import wraps
-import random , gc , math , pymysql , smtplib , ssl
+import  gc , pymysql
 from datetime import timedelta, date
+
 
 def database():
     db = pymysql.connect(host='vacation-planner.mysql.database.azure.com', user='sundasnoreen@vacation-planner',
@@ -66,6 +67,7 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/employees_list')
+@login_required
 def Employee():
     rows = ''
     conn = database()
@@ -81,6 +83,7 @@ def Employee():
     return render_template('Employee.html',val="Employees",user=session['name'],rows=rows)
 
 @app.route('/update_Employees')
+@login_required
 def Up_Employee():
     rows = ''
     conn = database()
@@ -96,6 +99,7 @@ def Up_Employee():
     return render_template('Update.html',user=session['name'],rows=rows)
 
 @app.route('/add_Employees', methods=['GET', 'POST'])
+@login_required
 def Add_Employee():
     conn = database()
     cursor = conn.cursor()
@@ -117,6 +121,7 @@ def Add_Employee():
     return render_template('AddEmployee.html',val="Employee",user=session['name'],error=error)
 
 @app.route('/update_<id>_<name>_<cnic>_<num>_<ad>', methods=['GET', 'POST'])
+@login_required
 def Update_Employee(id,name,cnic,num,ad):
     conn = database()
     cursor = conn.cursor()
@@ -138,6 +143,7 @@ def Update_Employee(id,name,cnic,num,ad):
     return render_template('UpdateEmployee.html',val="Employee",user=session['name'],name=name,cnic=cnic,num=num,ad=ad,error=error)
 
 @app.route('/clients_list')
+@login_required
 def Client():
     rows = ''
     conn = database()
@@ -153,6 +159,7 @@ def Client():
     return render_template('Employee.html',val="Clients",user=session['name'],rows=rows)
 
 @app.route('/update_Clients')
+@login_required
 def Up_Client():
     rows = ''
     conn = database()
@@ -168,6 +175,7 @@ def Up_Client():
     return render_template('UpdateCl.html',user=session['name'],rows=rows)
 
 @app.route('/add_Clients', methods=['GET', 'POST'])
+@login_required
 def Add_Client():
     conn = database()
     cursor = conn.cursor()
@@ -189,6 +197,7 @@ def Add_Client():
     return render_template('AddEmployee.html',val="Client",user=session['name'],error=error)
 
 @app.route('/update_client_<id>_<name>_<cnic>_<num>_<ad>', methods=['GET', 'POST'])
+@login_required
 def Update_Client(id,name,cnic,num,ad):
     conn = database()
     cursor = conn.cursor()
@@ -208,6 +217,394 @@ def Update_Client(id,name,cnic,num,ad):
         finally:
             conn.close()
     return render_template('UpdateEmployee.html',val="Client",user=session['name'],name=name,cnic=cnic,num=num,ad=ad,error=error)
+
+@app.route('/work')
+@login_required
+def Work():
+    return render_template('Work.html',user=session['name'])
+
+@app.route('/new_work', methods=['GET', 'POST'])
+@login_required
+def NewWork():
+    rows = ''
+    error=''
+    d1 = date.today()
+    conn = database()
+    cursor = conn.cursor()
+    sql = 'SELECT * FROM `client` ORDER BY `Name`'
+    sql1 = 'SELECT * FROM `employee` ORDER BY `Name`'
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.execute(sql1)
+    cols = cursor.fetchall()
+    if request.method == 'POST':
+        try:
+            Client = request.form['client']
+            Employee = request.form['employee']
+            Date = request.form['date']
+            Store = request.form['store']
+            Pur = request.form['pur']
+            Amount = request.form['amount']
+            Bill = request.form['cli']
+            sql = 'INSERT INTO `work`(`Client`, `Employee`, `Store`, `Purchased`, `Amount`, `Bill`, `Date`) VALUES (%s,%s,%s,%s,%s,%s,%s)'
+            cursor.execute(sql, (Client,Employee,Store,Pur,Amount,Bill,Date))
+            conn.commit()
+            sql1='SELECT`Balance` FROM `employee` WHERE `ID`=%s'
+            cursor.execute(sql1,(Employee))
+            value=cursor.fetchall()
+            for a in value:
+                emp_am=a[0]
+            emp_am=int(emp_am)+int(Amount)
+            sql2 = 'UPDATE `employee` SET `Balance`=%s WHERE `ID`=%s'
+            cursor.execute(sql2, (emp_am,Employee))
+            conn.commit()
+            sql3= 'SELECT`Balance` FROM `client` WHERE `ID`=%s'
+            cursor.execute(sql3, (Client))
+            values = cursor.fetchall()
+            for a in values:
+                cl_am = a[0]
+            cl_am = int(cl_am) + int(Bill)
+            sql4 = 'UPDATE `client` SET `Balance`=%s WHERE `ID`=%s'
+            cursor.execute(sql4, (cl_am, Client))
+            conn.commit()
+            error = "Work Executed Successfully."
+        except Exception as e:
+            print(e)
+            error = "There seems to be some Problem , Please Try Again."
+        finally:
+            conn.close()
+    return render_template('NewWork.html',user=session['name'],rows=rows,cols=cols,d1=d1,error=error)
+
+@app.route('/monthly_<month>_<mon>')
+@login_required
+def Details(month,mon):
+    Store=[]
+    Sum=0
+    Pur=0
+    Purchased=[]
+    Amount = []
+    Bill = []
+    Date=[]
+    Empl=[]
+    Employee=[]
+    Client=[]
+    rows = ''
+    conn = database()
+    cursor = conn.cursor()
+    sql = 'SELECT * FROM `work` ORDER BY `Date` DESC'
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for i in rows:
+            a = str(i[7])
+            year, mo, date1 = a.split('-')
+            m=year+"-"+mo
+            if m==month:
+                Store.append(i[3])
+                Purchased.append(i[4])
+                Amount.append(i[5])
+                Bill.append(i[6])
+                Date.append(i[7])
+                sql1 = 'SELECT `Name` FROM `employee` WHERE `ID`=%s'
+                cursor.execute(sql1, (i[2]))
+                val = cursor.fetchall()
+                for a in val:
+                    Employee.append(a[0])
+                sql2 = 'SELECT `Name` FROM `client` WHERE `ID`=%s'
+                cursor.execute(sql2, (i[1]))
+                val2 = cursor.fetchall()
+                for a in val2:
+                    Client.append(a[0])
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    for i in Bill:
+        Sum=Sum+i
+    for i in Amount:
+        Pur=Pur+i
+    values=zip(Employee,Client,Store,Purchased,Amount,Bill,Date)
+    return render_template('WorkHistory.html',mon=mon,Pur=Pur,user=session['name'],rows=values,Sum=Sum)
+
+@app.route('/employee_balance')
+@login_required
+def Employee_Balance():
+    rows = ''
+    conn = database()
+    cursor = conn.cursor()
+    sql='SELECT * FROM `employee` ORDER BY `Name` DESC'
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    return render_template('Employee_Balance.html',user=session['name'],rows=rows)
+
+@app.route('/pay_emp_<id>_<bal>_<name>', methods=['GET', 'POST'])
+@login_required
+def Pay_Employee(id,bal,name):
+    conn = database()
+    cursor = conn.cursor()
+    error=''
+    left=0
+    if request.method == 'POST':
+        try:
+            Pending=bal
+            Paying = request.form['paying']
+            if int(Paying)>int(Pending):
+                error = "You are paying more than the Pending Amount."
+            else:
+                left = int(Pending)-int(Paying)
+                sql2 = 'UPDATE `employee` SET `Balance`=%s WHERE `ID`=%s'
+                cursor.execute(sql2, (left, id))
+                conn.commit()
+                error = "Balance Updated. Pending Amount Now is " + str(left) + " PKR."
+
+        except:
+            error = "There seems to be some Problem , Please Try Again."
+        finally:
+            conn.close()
+    return render_template('Pay_Emp.html',val="Employee's",user=session['name'],bal=bal,name=name,error=error)
+
+@app.route('/client_balance')
+@login_required
+def Client_Balance():
+    rows = ''
+    conn = database()
+    cursor = conn.cursor()
+    sql='SELECT * FROM `client` ORDER BY `Name` DESC'
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    return render_template('Client_Balance.html',user=session['name'],rows=rows)
+
+@app.route('/pay_cli_<id>_<bal>_<name>', methods=['GET', 'POST'])
+@login_required
+def Pay_Client(id,bal,name):
+    conn = database()
+    cursor = conn.cursor()
+    error=''
+    left=0
+    if request.method == 'POST':
+        try:
+            Pending=bal
+            Paying = request.form['paying']
+            if int(Paying)>int(Pending):
+                error = "Client is paying more than the Pending Amount."
+            else:
+                left = int(Pending)-int(Paying)
+                sql2 = 'UPDATE `client` SET `Balance`=%s WHERE `ID`=%s'
+                cursor.execute(sql2, (left, id))
+                conn.commit()
+                error = "Balance Updated. Pending Amount Now is " + str(left) + " PKR."
+
+        except:
+            error = "There seems to be some Problem , Please Try Again."
+        finally:
+            conn.close()
+    return render_template('Pay_Emp.html',val="Client's",user=session['name'],bal=bal,name=name,error=error)
+
+@app.route('/Employees_<id>_<name>')
+@login_required
+def ID_Em(id,name):
+    Store = []
+    Purchased = []
+    Amount = []
+    Bill = []
+    Date = []
+    Empl = []
+    Client = []
+    rows=''
+    error=''
+    conn = database()
+    cursor = conn.cursor()
+    try:
+        sql = 'SELECT * FROM `work` WHERE `Employee`=%s ORDER BY `Date` DESC'
+        cursor.execute(sql,(id))
+        rows = cursor.fetchall()
+        for i in rows:
+            Store.append(i[3])
+            Purchased.append(i[4])
+            Amount.append(i[5])
+            Bill.append(i[6])
+            Date.append(i[7])
+            sql1 = 'SELECT `Name` FROM `client` WHERE `ID`=%s'
+            cursor.execute(sql1, (i[1]))
+            val = cursor.fetchall()
+            for a in val:
+                Client.append(a[0])
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    info = zip(Client, Store, Purchased, Amount, Bill, Date)
+    return render_template('Single.html',Name=name,val="Client",user=session['name'],rows=info,name=name,error=error)
+
+@app.route('/Clients_<id>_<name>')
+@login_required
+def ID_Cl(id,name):
+    Store = []
+    Purchased = []
+    Amount = []
+    Bill = []
+    Date = []
+    Client = []
+    rows=''
+    error=''
+    conn = database()
+    cursor = conn.cursor()
+    try:
+        sql = 'SELECT * FROM `work` WHERE `Client`=%s ORDER BY `Date` DESC'
+        cursor.execute(sql,(id))
+        rows = cursor.fetchall()
+        for i in rows:
+            Store.append(i[3])
+            Purchased.append(i[4])
+            Amount.append(i[5])
+            Bill.append(i[6])
+            Date.append(i[7])
+            sql1 = 'SELECT `Name` FROM `employee` WHERE `ID`=%s'
+            cursor.execute(sql1, (i[2]))
+            val = cursor.fetchall()
+            for a in val:
+                Client.append(a[0])
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    info = zip(Client, Store, Purchased, Amount, Bill, Date)
+    return render_template('Single.html',Name=name,val="Employee",user=session['name'],rows=info,name=name,error=error)
+
+@app.route('/work_history',methods=['GET', 'POST'])
+@login_required
+def History():
+    Store=[]
+    Values=[]
+    mon=''
+    rows = ''
+    conn = database()
+    cursor = conn.cursor()
+    sql='SELECT * FROM `work` ORDER BY `Date` DESC'
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for i in rows:
+            a=str(i[7])
+            year,mo,date1=a.split('-')
+            if mo=='01':
+                month="January"
+            elif mo=='02':
+                month="February"
+            elif mo=='03':
+                month="March"
+            elif mo=='04':
+                month="April"
+            elif mo=='05':
+                month="May"
+            elif mo == '06':
+                month = "June"
+            elif mo == '07':
+                month = "July"
+            elif mo == '08':
+                month = "August"
+            elif mo == '09':
+                month = "September"
+            elif mo == '10':
+                month = "October"
+            elif mo == '11':
+                month = "November"
+            else:
+                month="December"
+            vals=month+" "+year
+            if vals in Store:
+                pass
+            else:
+                Store.append(vals)
+                Values.append(year+"-"+mo)
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    info=zip(Store,Values)
+    if request.method=='POST':
+        months=request.form['month']
+        for Values,Store in info:
+            print(Values,Store)
+            if Store==months:
+                print(Values+Store)
+                mon=Values
+            print("He"+mon)
+        return redirect(url_for('Details',month=months,mon=mon))
+    return render_template('Select_Month.html',user=session['name'],rows=info)
+
+@app.route('/settings')
+@login_required
+def Settings():
+    return render_template('settings.html',user=session['name'])
+
+@app.route('/admins')
+@login_required
+def Admins():
+    cat=session['category']
+    if cat=="Main":
+        return redirect(url_for('Admins_Det'))
+    else:
+        return render_template('Message.html')
+    return render_template('settings.html',user=session['name'])
+
+@app.route('/admins_details')
+@login_required
+def Admins_Det():
+    rows = ''
+    conn = database()
+    cursor = conn.cursor()
+    sql = 'SELECT * FROM `admins` ORDER BY `Name`'
+    try:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+    except:
+        error = " Unable to fetch data."
+    finally:
+        conn.close()
+    return render_template('Admins.html', user=session['name'], rows=rows)
+
+@app.route('/password', methods=['GET', 'POST'])
+@login_required
+def Password():
+    error = ''
+    username = session['user']
+    Password = session['password']
+    conn = database()
+    cursor = conn.cursor()
+    try:
+        if request.method == 'POST':
+            Pre = request.form['pre']
+            New = request.form['new']
+            repeat = request.form['repeat']
+            if repeat != New:
+                error = "Passwords doesn't match."
+            elif Pre != Password:
+                error = 'Please Enter Your Correct Current Password.'
+            elif Pre == New:
+                error = 'You are using your Current Password.'
+            elif repeat == New and Pre == Password:
+                sql = 'UPDATE `admins` SET `Password`=%s WHERE `Login`=%s'
+                cursor.execute(sql, (New, username))
+                conn.commit()
+                return redirect(url_for('logout'))
+            else:
+                error = "Password couldn't be changed.Please Try Again Later."
+    except:
+        error = "There Seems to be some Problem, Please Try Again."
+    finally:
+        conn.close()
+    return render_template('password.html', error=error, user=session['name'])
 
 app.debug=True
 app.run()
